c3675c928cee4c645104011765a8773338a915fb
Will Bainton <baintonw@gmail.com>
2019-11-27 16:46:19 -0500

    clicking revels bombs

--- source.js
+++ source.js
@@ -1,9 +1,23 @@
 const cells = [...document.querySelectorAll('.cell')]
 
 function randomIndex() {
-  let randomI = (Math.floor(Math.random() * 256))
-  console.log('this is a cell: ', cells[randomI], 'this is its index: ', cells.indexOf(cells[randomI]))
-  cell = cells[randomI]
-  cell.style = "background: red"
-
+  // console.log('this is a cell: ', cells[randomI], 'this is its index: ', cells.indexOf(cells[randomI]))
+  let i = 0
+  while (i < 40) {
+    let randomI = (Math.floor(Math.random() * 256))
+    cell = cells[randomI]
+    if(cell.innerHTML !== `<div class="bomb none">X</div>`){
+      cell.innerHTML = `<div class="bomb none">X</div>`
+      i++
+      console.log(i)
+    }
+  }
 }
+
+cells.forEach(cell => {
+  cell.addEventListener('click', function(){
+    console.log('this cell: ', this, this)
+    cell.classList.remove('covered')
+    cell.innerHTML = `<div class="bomb">X</div>`
+  })
+})
