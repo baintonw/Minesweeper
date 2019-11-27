c213504852412de33bcc239458b1d3b504d7230a
Will Bainton <baintonw@gmail.com>
2019-11-27 14:26:32 -0500

    grid template added

--- gridtest/grid.js
+++ gridtest/grid.js
@@ -1,11 +1,29 @@
 const grid = document.querySelector('.grid')
-
+//
 function makeGrid(x, y, container) {
 
   console.log(x, y, container)
     for(let i = 0; i < (x * y); i++) {
       container.innerHTML +=`
-        <button style="height:${container.offsetHeight / x}px; width:${container.offsetWidth / y}px"></button>
+        <button style="height: ${container.offsetWidth / x}px; width: ${container.offsetHeight / y}px">x</button>
       `
     }
+}
+
+function logGrid() {
+  xAxis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+  yAxis = [1, 2, 3, 4, 5, 6, 7, 8]
+
+  let row = []
+
+  for(let i = 0; i < xAxis.length; i++) {
+
+    for(let j = 0; j < yAxis.length; j++) {
+      row.push(xAxis[i] + yAxis[j])
+    }
+    console.log(row)
+
+    row = []
+  }
+
 }
